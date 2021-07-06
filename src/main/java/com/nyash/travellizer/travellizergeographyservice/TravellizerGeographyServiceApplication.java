284f22739c7704f505085be1466c03303eb0b733
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-06 07:09:08 +0700

    Initial commit

---
+++ src/main/java/com/nyash/travellizer/travellizergeographyservice/TravellizerGeographyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizergeographyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellizerGeographyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellizerGeographyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/travellizer/travellizergeographyservice/TravellizerGeographyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizergeographyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellizerGeographyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
